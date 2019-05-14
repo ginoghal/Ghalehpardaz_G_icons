167307ba1ac6ad1069f5dacc8fad2ee471bc64c0
Gino Ghalehpardaz <ginoghalehpardaz@Ginos-MBP-2.fc.ca>
2019-05-14 18:01:01 -0400

    some updated elements

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 () => {
   //stub
   console.log('fired!');
+
+  //select elements with Javacscript
+
+  //setting up a variable using the let keyword,
+  // and using a CSS selector to find the element we want to use.
+
+  //let stores the reference to element asa a variable (in memory)
+
+  let svgGraphic = document.querySelector("#badgeSVG");
+
+  // functions are reusable pieces of code
+  //that you can run at any time
+
+  function logMyID(){
+    console.log(this.id);
+  }
+
+    this.style.opacity=0.5%;
+
+svgGraphic.addEventListener("click", logMyID);
+
 })();
